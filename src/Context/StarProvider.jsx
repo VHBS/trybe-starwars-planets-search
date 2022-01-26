a54273328f6e6696bcf548da8034c2ff74b343a5
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-01-25 22:13:07 -0300

    Requisito 3

--- src/Context/StarProvider.jsx
+++ src/Context/StarProvider.jsx
@@ -4,11 +4,23 @@ import React, { useState } from 'react';
 import StarContext from './StarContext';
 
 export default function StarProvider({ children }) {
+  // Salva a requisição no estado
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState('');
-  const [renderFilter, setRenderFilter] = useState(false);
 
-  const context = { data, setData, filter, setFilter, renderFilter, setRenderFilter };
+  // Filtra o arquivo data salvo em outro estado.
+  const [filterName, setFilterName] = useState('');
+
+  // Number filters
+  const [filtroNumero, setFiltroNumero] = useState([]);
+
+  const context = {
+    data,
+    setData,
+    filterName,
+    setFilterName,
+    filterByNumericValues: filtroNumero,
+    setFiltroNumero,
+  };
 
   return (
     <StarContext.Provider value={ context }>
--- src/components/TableFilter.jsx
+++ src/components/TableFilter.jsx
@@ -1,12 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import StarContext from '../Context/StarContext';
 
 export default function TableFilter() {
-  const { data, filter } = useContext(StarContext);
+  const { data, filterName, filterByNumericValues } = useContext(StarContext);
+  const [filterDone, setFilterDone] = useState([]);
+
+  useEffect(() => {
+    const filterNumeric = () => {
+      if (filterByNumericValues.length === 0) {
+        setFilterDone(data);
+      } else {
+        filterByNumericValues.forEach((filtro) => {
+          const teste = data
+            .filter((item) => {
+              if (filtro.comparison === 'maior que') {
+                return Number(item[filtro.column]) > Number(filtro.value);
+              } if (filtro.comparison === 'menor que') {
+                return Number(item[filtro.column]) < Number(filtro.value);
+              }
+              return Number(item[filtro.column]) === Number(filtro.value);
+            });
+          setFilterDone(teste);
+        });
+      }
+    };
+
+    // Number(filtro.value)
+    // > Number(item[filtro.column]
+
+    filterNumeric();
+  }, [data, filterByNumericValues]);
 
   return (
     <tbody>
-      {data.filter((item) => item.name.includes(filter))
+      { filterDone.filter((item) => item.name.includes(filterName))
         .map((item, index) => (
           <tr key={ item.name + index }>
             <td>{item.name}</td>
